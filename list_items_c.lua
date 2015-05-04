ea8be09e6db7daa66bac4b67dc6b0d7dd88430c2
StanleySathler <stanleysathlerpinto@gmail.com>
2015-05-03 21:11:33 -0300

    Weapons, food, medics and clothing items refactored
    List of weapons, food, medics and clothing items were refactored on this
    commit. But they are not finished yet, I'm sure that we will have to add
    some things later.

---
+++ list_items_c.lua
@@ -0,0 +1,104 @@
+gameMedicItems = {
+	["MedicKit"] = {
+		["item_name"] = "Kit Médico",
+		["max_health"] = 12000,
+		["item_slots_size"] = 2,
+	},
+	["Bandage"] = {
+		["item_name"] = "Band-aid",
+		["max_health"] = 0, -- normally does not give health
+		["item_slots_size"] = 0.5,
+	},
+	["Morphine"] = {
+		["item_name"] = "Morfina",
+		["max_health"] = 0, -- normally does not give health
+		["item_slots_size"] = 1,
+	},
+	["Painkiller"] = {
+		["item_name"] = "Analgésico",
+		["max_health"] = 0, -- normally does not give health
+		["item_slots_size"] = 0.5,
+	},
+}
+
+gameFoodItems = {
+	["Burger"] = {
+		["item_name"] = "Hambúrguer",
+		["max_health"] = 600,
+		["max_restore"] = 80,
+		["item_slots_size"] = 1,
+	},
+	["Pizza"] = {
+		["item_name"] = "Pizza",
+		["max_health"] = 800, -- normally does not give health
+		["max_restore"] = 100,
+		["item_slots_size"] = 1,
+	},
+	["CookedMeat"] = {
+		["item_name"] = "Carne Cozida",
+		["max_health"] = 1200, -- normally does not give health
+		["max_restore"] = 100,
+		["item_slots_size"] = 1,
+	},
+	["RawMeat"] = {
+		["item_name"] = "Carne Crua",
+		["max_health"] = 600, -- normally does not give health
+		["max_restore"] = 70,
+		["item_slots_size"] = 1,
+	},
+	["BeansCan"] = {
+		["item_name"] = "Feijão Enlatado",
+		["max_health"] = 400, -- normally does not give health
+		["max_restore"] = 60,
+		["item_slots_size"] = 1,
+	},
+	["PastaCan"] = {
+		["item_name"] = "Lasanha Pronta",
+		["max_health"] = 600, -- normally does not give health
+		["max_restore"] = 90,
+		["item_slots_size"] = 1,
+	},
+	["FullWaterBottle"] = {
+		["item_name"] = "Garrafa D'água",
+		["max_health"] = 600, -- normally does not give health
+		["max_restore"] = 100,
+		["item_slots_size"] = 1,
+	},
+	["EmptyWaterBottle"] = {
+		["item_name"] = "Garrafa D'água (vazia)",
+		["max_health"] = 0, -- normally does not give health
+		["max_restore"] = 0,
+		["item_slots_size"] = 1,
+	},
+	["FullSodaCan"] = {
+		["item_name"] = "Lata de Suco",
+		["max_health"] = 600, -- normally does not give health
+		["max_restore"] = 100,
+		["item_slots_size"] = 1,
+	},
+	["EmptySodaCan"] = {
+		["item_name"] = "Lata de Suco (vazia)",
+		["max_health"] = 0, -- normally does not give health
+		["max_restore"] = 0,
+		["item_slots_size"] = 1,
+	},
+}
+
+gameClothingItems = {
+	["Survivor"] = {
+		["item_name"] = "Roupa de Sobrevivente",
+		["item_slots_size"] = 1,
+	},
+	["Civilian"] = {
+		["item_name"] = "Roupa de Civil",
+		["item_slots_size"] = 1,
+	},
+	["GhillieDesert"] = {
+		["item_name"] = "Ghillie Suit (Deserto)",
+		["item_slots_size"] = 1,
+	},
+	["GhillieForest"] = {
+		["item_name"] = "Ghillie Suit (Floresta)",
+		["item_slots_size"] = 1,
+	},
+}
---
+++ list_weapons_c.lua
@@ -0,0 +1,169 @@
+-- ------------------------------------------------------------
+-- This list of Weapons contains all fire weapons and their 
+-- settings, like max of bullets, damage, and others.
+-- ------------------------------------------------------------
+gameFireWeapons = {
+	["Shotgun"] = {
+		["item_name"] = "Escopeta",
+		["magazine_name"] = "Munição de Escopeta",
+		["max_bullets_on_mag"] = 7,
+		["max_damage"] = 4500,
+		["item_slots_size"] = 2,
+		["magazine_slots_size"] = 0.5
+	},
+	["SawnOff"] = {
+		["item_name"] = "Sawn-Off",
+		["magazine_name"] = "Munição de Sawn-Off",
+		["max_bullets_on_mag"] = 2,
+		["max_damage"] = 4500,
+		["item_slots_size"] = 2,
+		["magazine_slots_size"] = 0.5
+	},
+	["SPAS12"] = {
+		["item_name"] = "SPAS-12",
+		["magazine_name"] = "Munição de SPAS-12",
+		["max_bullets_on_mag"] = 7,
+		["max_damage"] = 4500,
+		["item_slots_size"] = 7,
+		["magazine_slots_size"] = 0.5
+	},
+	["M4"] = {
+		["item_name"] = "M4",
+		["magazine_name"] = "Munição de M4",
+		["max_bullets_on_mag"] = 50,
+		["max_damage"] = 3000,
+		["item_slots_size"] = 2,
+		["magazine_slots_size"] = 0.5
+	},
+	["AK47"] = {
+		["item_name"] = "AK-47",
+		["magazine_name"] = "Munição de AK-47",
+		["max_bullets_on_mag"] = 30,
+		["max_damage"] = 3500,
+		["item_slots_size"] = 2,
+		["magazine_slots_size"] = 0.5
+	},
+	["Sniper"] = {
+		["item_name"] = "Rifle Sniper",
+		["magazine_name"] = "Munição de Sniper",
+		["max_bullets_on_mag"] = 5,
+		["max_damage"] = 12000,
+		["item_slots_size"] = 2,
+		["magazine_slots_size"] = 0.5
+	},
+	["CountryRifle"] = {
+		["item_name"] = "Country Rifle",
+		["magazine_name"] = "Munição de Country R.",
+		["max_bullets_on_mag"] = 10,
+		["max_damage"] = 8000,
+		["item_slots_size"] = 2,
+		["magazine_slots_size"] = 0.5
+	},
+	["Pistol"] = {
+		["item_name"] = "Pistola",
+		["magazine_name"] = "Munição de Pistola",
+		["max_bullets_on_mag"] = 17,
+		["max_damage"] = 900,
+		["item_slots_size"] = 1,
+		["magazine_slots_size"] = 0.25
+	},
+	["Pistol"] = {
+		["item_name"] = "Pistola",
+		["magazine_name"] = "Munição de Pistola",
+		["max_bullets_on_mag"] = 17,
+		["max_damage"] = 900,
+		["item_slots_size"] = 1,
+		["magazine_slots_size"] = 0.25
+	},
+	["SilencedPistol"] = {
+		["item_name"] = "Pistola Silenciosa",
+		["magazine_name"] = "Munição de P. Silenciosa",
+		["max_bullets_on_mag"] = 17,
+		["max_damage"] = 800,
+		["item_slots_size"] = 1,
+		["magazine_slots_size"] = 0.25
+	},
+	["DesertEagle"] = {
+		["item_name"] = "Desert Eagle",
+		["magazine_name"] = "Munição de Desert",
+		["max_bullets_on_mag"] = 7,
+		["max_damage"] = 3000,
+		["item_slots_size"] = 1,
+		["magazine_slots_size"] = 0.25
+	},
+	["Uzi"] = {
+		["item_name"] = "Uzi",
+		["magazine_name"] = "Munição de Uzi",
+		["max_bullets_on_mag"] = 30,
+		["max_damage"] = 900,
+		["item_slots_size"] = 1,
+		["magazine_slots_size"] = 0.5
+	},
+	["MP5"] = {
+		["item_name"] = "MP5",
+		["magazine_name"] = "Munição de MP5",
+		["max_bullets_on_mag"] = 30,
+		["max_damage"] = 1200,
+		["item_slots_size"] = 2,
+		["magazine_slots_size"] = 0.5
+	},
+}
+
+
+-- ------------------------------------------------------------
+-- This list of weapons contains all white weapons and their 
+-- settings, like max of bullets, damage, and others.
+-- ------------------------------------------------------------
+gameWhiteWeapons = {
+	["Knife"] = {
+		["item_name"] = "Faca",
+		["max_damage"] = 1000,
+		["item_slots_size"] = 1,
+	},
+	["Katana"] = {
+		["item_name"] = "Machado",
+		["max_damage"] = 1200,
+		["item_slots_size"] = 2,
+	},
+	["BaseballBat"] = {
+		["item_name"] = "Taco de Beisebol",
+		["max_damage"] = 900,
+		["item_slots_size"] = 2,
+	},
+	["GolfBat"] = {
+		["item_name"] = "Taco de Golf",
+		["max_damage"] = 900,
+		["item_slots_size"] = 2,
+	},
+	["Shovel"] = {
+		["item_name"] = "Pá",
+		["max_damage"] = 900,
+		["item_slots_size"] = 2,
+	},
+	["TearGas"] = {
+		["item_name"] = "Gás Lacrimogêneo",
+		["max_damage"] = 300,
+		["item_slots_size"] = 1,
+	},
+	["Grenade"] = {
+		["item_name"] = "Granada",
+		["max_damage"] = 12000,
+		["item_slots_size"] = 1,
+	},
+}
+
+
+-- ------------------------------------------------------------
+-- This list of weapons contains all white weapons and their 
+-- settings, like max of bullets, damage, and others.
+-- ------------------------------------------------------------
+gameOtherWeapons = {
+	["Parachute"] = {
+		["item_name"] = "Paraquedas",
+		["item_slots_size"] = 1,
+	},
+	["Binoculars"] = {
+		["item_name"] = "Binóculos",
+		["item_slots_size"] = 1,
+	},
+}
